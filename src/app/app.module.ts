3941be5639d8fc70f35fb32487356c69a4bbf105
Livia Brandao <liviatoledo27@gmail.com>
2024-11-04 20:59:01 -0300

    "add directives"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
 import { ProductComponent } from './views/product/product.component';
+import { RedDirective } from './directives/red.directive';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { ProductComponent } from './views/product/product.component';
     NavComponent,
     HomeComponent,
     ProductComponent,
+    RedDirective,
   ],
   imports: [
     BrowserModule,
